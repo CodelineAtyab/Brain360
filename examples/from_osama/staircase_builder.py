481a9249b6ff5549612d5ae0ba1c59c8ea606076
Osama Said Al Zakwani <Osama.Zakwani@gmail.com>
2025-05-20 15:35:26 +0400

    v2

--- examples/from_osama/staircase_builder.py
+++ examples/from_osama/staircase_builder.py
@@ -2,11 +2,13 @@ input_number = int(input("Enter the Positive Number: "))
 
 if input_number > 0:
     for i in range(input_number):
-        for j in range(i+1):
+
+        for s in range(input_number - i - 1):
+            print(" ", end="")
+
+        for j in range(i + 1):
             print("#", end="")
         print("")
 
-
 else:
     print("[Error]: Add the Positive Number Please")
-    input_number = int(input("Enter the Positive Number: "))
