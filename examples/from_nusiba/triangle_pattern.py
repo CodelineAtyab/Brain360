7d725150a94a7082d9dd3a6dea895c21abeadafe
Nusiba <nusibaalnabhani@gmail.com>
2025-05-20 15:42:44 +0400

    test

--- examples/from_nusiba/triangle_pattern.py
+++ examples/from_nusiba/triangle_pattern.py
@@ -1,6 +1,4 @@
 num_of_rows = int(input("Enter the number of rows:"))
 
 for i in range(1, num_of_rows + 1):
-    for j in range(1, i + 1):
-        print(j, end=" ")
-    print()
+    print("1" * i)
