733db4097f9eed34a954c860c76028a965475a62
Janna-Khalid <janna.balushi@outlook.com>
2025-05-20 21:19:39 +0400

    Num Seq Generator

---
+++ examples/from_Janna/Number_Seq_Generator.py
@@ -0,0 +1,8 @@
+x = int(input("Please Enter the Number of Rows (Positive Value!): "))
+for i in range(1,x + 1):
+    for j in range(1,i + 1):
+        print(j, end="")
+    for j in range(i - 1,0,-1):
+        print(j, end="")
+    print()
+
