b039907f1e7171ad07eb6d5f50e1fad4ca83ab62
mahayhmadi <mahayhmadi@gmail.com>
2025-05-21 15:33:41 +0400

    traingle patern

---
+++ examples/from_maha/Print_Triangle_Pattern.py
@@ -0,0 +1,8 @@
+num_of_row = int(input("Enter the number of rows"))
+
+if num_of_row > 0:
+    for x in range(1, num_of_row+1):
+        print("1"*x)
+    else:
+        print("Enter a number more than 0")
+
