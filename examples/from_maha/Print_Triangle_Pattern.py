90071aeebc73d3351f67d8b17eefb9ef89e9427e
mahayhmadi <mahayhmadi@gmail.com>
2025-05-21 14:06:36 +0400

    Done

--- examples/from_maha/Print_Triangle_Pattern.py
+++ examples/from_maha/Print_Triangle_Pattern.py
@@ -2,6 +2,6 @@ num_of_row = int(input("Enter the number of rows"))
 
 if num_of_row > 0:
     for x in range(1, num_of_row+1):
-        print("*"*x)
+        print("1"*x)
     else:
         print("Enter a number more than 0")
