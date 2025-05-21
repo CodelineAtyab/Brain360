460d535b8d7d64f7fa829d43ca70da20ffe91e7c
Janna-Khalid <janna.balushi@outlook.com>
2025-05-21 15:16:20 +0400

    deleted files

--- examples/from_Janna/Staircase_Builder.py
+++
@@ -1,6 +0,0 @@
-x = int(input("Enter the Height of the Staircase (Positive Values Only): "))
-if x > 0:
-    for i in range(x + 1):
-        print(" " * (x - i) + "#" * i)
-else:
-    print("Invalid Input!")
