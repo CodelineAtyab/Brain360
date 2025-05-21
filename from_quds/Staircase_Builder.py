a19e81a7097e628d62562db096bd9ffc4f9f8b0a
Quds-Aljabri <qudsahmedaljabri@gmail.com>
2025-05-21 09:31:49 +0400

    Print staircase builder

--- from_quds/Staircase_Builder.py
+++ from_quds/Staircase_Builder.py
@@ -1,3 +1,6 @@
-num_of_rows = int(input("Enter the number of rows: ")) # Asking the user to enter the number of rows
-for i in range(1, num_of_rows + 1 ):   
-    print(" " * (num_of_rows - i) + "#" * i)
+num_of_rows = int(input("Enter the number of rows: ")) 
+if num_of_rows > 0:
+    for i in range(1, num_of_rows + 1 ):   
+        print(" " * (num_of_rows - i) + "#" * i)
+else:
+    print("Invalid Input")
