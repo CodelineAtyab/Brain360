056e5d2c8a690da35e8995583863d61a38bc6fa1
Hajer603 <hajeralhattali1@gmail.com>
2025-05-21 12:06:06 +0400

    Numer sequence generator

---
+++ examples/from Hajer/Number Sequence Generator.py
@@ -0,0 +1,13 @@
+rows=int(input("Enter the number of rwos = "))
+
+for row in range(1, rows + 1):
+
+    for num in range(1, row + 1):
+
+        print(num, end="")
+
+    for num in range(row - 1, 0, -1):
+
+        print(num, end="")
+        
+    print("")
