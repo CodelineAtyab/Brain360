e7c53e5f17c8de8d2f77f94f6fbb89c186e16c26
tomh979 <engineertomh@gmail.com>
2025-05-22 10:39:54 +0400

    done

--- examples/from_fatma_salim/shopping_list.py
+++ examples/from_fatma_salim/shopping_list.py
@@ -1,8 +1,10 @@
 shopping_list=[]
 
-print("\n1. add item") # \n for new line 
-print("2. show list ")
-print("3. exit ")
+print ("\n1. add item") # \n for new line 
+print ("2. remove item")
+print ("3. show list ")
+print ("4. clear list ")
+print ("5. exit ")
 
 while True :
    
@@ -13,11 +15,34 @@ while True :
       print (" item added ! ")
 
     elif choice == "2":
-       print ("shopping list :")
-       for item in shopping_list:
-        print ("-" + item)
+        if shopping_list:
+            for i in range(len(shopping_list)):
+                print(f"{i + 1}. {shopping_list[i]}")
+            index = int(input("Enter item number to remove: "))
+            if 1 <= index <= len(shopping_list):
+                removed = shopping_list.pop(index - 1) # pop is used to remove item from shopping list  
+                print(f"'{removed}' removed from the list.")
+            else:
+                print("Invalid number.") 
+         
+        else:
+            print("List is empty.")
 
     elif choice == "3":
+        if shopping_list:
+            print("Your shopping list:")
+            for i in range(len(shopping_list)):
+                print(f"{i + 1}. {shopping_list[i]}")
+        else:
+            print("List is empty.")
+
+
+
+    elif choice == "4":
+        shopping_list = []
+        print("List cleared.")
+
+    elif choice == "5":
        print (" goodbye !")
        break 
     else :
