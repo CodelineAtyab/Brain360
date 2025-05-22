4bda786c6d6c6997724c83c8ecb632015cd1c21c
Malik <malek_noor40@hotmail.com>
2025-05-22 09:42:59 +0400

    done with shopping list

---
+++ examples/from_malik/shopping_list.py
@@ -0,0 +1,36 @@
+# shopping_list = []
+# while True:
+#     item = input("Enter your shopping list (or 'done' to finish): ")
+#     if item.lower() == 'done':
+#         break
+#     shopping_list.append(item)
+#     #Printing the list of numbers
+# #print("shopping list:", shopping_list)
+# print("Shopping List:")
+# count = 1
+# for item in shopping_list:
+#     print(count ,"." , item)
+#     count += 1
+
+
+    #I want the shoping list as a string 
+
+
+#     shopping_list = []
+# count = 1
+
+# while True:
+#     item = input("Enter item (or 'done' to finish): ")
+#     if item.lower() == 'done':
+#         break
+#     shopping_list.append(item)
+
+print("Welcome to Shopping List Manager!")
+# options = input("please choose one of the following \n 1. Add item \n 2. Remove item \n 3. Show list \n 4. Clear list \n 5. Exit)
+
+# print("please choose one of the following \n 1. Add item \n 2. Remove item \n 3. Show list \n 4. Clear list \n 5. Exit")       
+
+# options = input("p")
+# Choose an option: 1
+# Enter item to add: Milk
+# 'Milk' added to the list
