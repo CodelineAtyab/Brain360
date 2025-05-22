ba31503feff2e38ee61909964191582ccbbb937c
deena <deealbusaidi@gmail.com>
2025-05-22 10:46:32 +0400

    number sequence

---
+++ examples/from_Deena/Number_sequence.py
@@ -0,0 +1,8 @@
+number = int (input ("Input: "))
+
+for a in range (1, number + 1):
+    for b in range (1, a + 1 ):
+        print (b, end= '')
+    for b in range (a - 1, 0, -1 ):
+        print (b, end='')
+    print('')
