e19b61e4cb1ddf9992067fe28fdf2f9322ea5910
osama Said Al Zakwani <osama.zakwani@gmail.com>
2025-05-25 20:14:57 +0400

    The maximum value finder task is done and ready for reviwe

--- examples/from_osama/implement-maximum-value-finder-function.py
+++ examples/from_osama/implement-maximum-value-finder-function.py
@@ -7,11 +7,16 @@ def largest_number():
             large_number = i
     print(large_number)
 
-
 inputted_numbers = input("Enter the numbers when you are done write (done) to print the largest number you entered: ")
+if inputted_numbers == "done":
+    print(None)
+else:
+    database.append(int(inputted_numbers))
+
 
 while inputted_numbers != "done":
     inputted_numbers = input("Enter the numbers when you are done write (done) to print the largest number you entered: ")
     if inputted_numbers == "done":
         largest_number()
-    database.append(inputted_numbers)
+        break
+    database.append(int(inputted_numbers))
