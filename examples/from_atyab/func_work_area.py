4e93adaae4cc966c01fcf1078e016b56295b1bbc
Syed Atyab Hussain <syed.atyab.hussain.om@gmail.com>
2025-05-25 10:05:24 +0400

    Basics of python func

---
+++ examples/from_atyab/func_work_area.py
@@ -0,0 +1,26 @@
+# Declare
+x = 0
+number_1 = 0
+num_2 = 0
+
+def sum_two_numbers(n1, n2):
+  r = n1 + n2 + 5
+  return r
+
+
+# Input
+number_1 = int(input("Enter 1st Number: "))  # 5
+num_2 = int(input("Enter 2nd Number: "))  # 10
+
+# Process
+result = 0
+
+result = sum_two_numbers(result, number_1)  # 5
+result = sum_two_numbers(result, number_1)  # 10
+result = sum_two_numbers(result, number_1)  # 15
+result = sum_two_numbers(result, number_1)  # 20
+result = sum_two_numbers(result, number_1)  # 25
+
+# Output
+# print(result)
+print(x)
