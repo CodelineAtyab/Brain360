7150256ad2c5ae312586c65a3434b575cf1ce83e
Mahmood <msssals54321@gmail.com>
2025-05-25 15:59:58 +0400

    User Story: Implement List Reversal Function

---
+++ examples/from_mahmood/Implement_List_Reversal_Function.py
@@ -0,0 +1,14 @@
+my_list = []
+add_to_list =input("enter the items you want to add :")
+while add_to_list !="done":
+  my_list.append(add_to_list)
+  if add_to_list != "done":
+     add_to_list =input("enter the items you want to add :")
+
+def reverse_my_list():
+    return list(reversed(my_list))
+
+print("the reversed list is:", reverse_my_list())
+
+
+
