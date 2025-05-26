38f4a36e358c9d51735da4700a580331e1e942c6
unknown <ahmedalnaimee44@gmail.com>
2025-05-26 10:36:49 +0400

    saved for later

---
+++ examples/from_ahmed/Maximum_Value_Finder_Function.py
@@ -0,0 +1,14 @@
+
+
+num_list = []
+
+def maximum(original_list):
+    maxi_list = []
+    for num in original_list:
+        if num > num in original_list:
+            maxi_list.append(num)
+            return maxi_list
+
+user_input = int(input())
+new_list = maximum(num_list)
+print(new_list)
