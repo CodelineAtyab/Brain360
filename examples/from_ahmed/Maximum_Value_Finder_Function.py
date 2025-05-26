ee47ae06fc5f9df31613544a87864ca62410fccd
unknown <ahmedalnaimee44@gmail.com>
2025-05-26 12:08:04 +0400

    saved for later

--- examples/from_ahmed/Maximum_Value_Finder_Function.py
+++ examples/from_ahmed/Maximum_Value_Finder_Function.py
@@ -8,7 +8,19 @@ def maximum(original_list):
         if num > num in original_list:
             maxi_list.append(num)
             return maxi_list
+        
 
+max_num = maximum(num_list)
+print("Enter your numbers and type 'done' once finished")
 user_input = int(input())
-new_list = maximum(num_list)
-print(new_list)
+
+if user_input == '':
+    print(None)
+else:
+    while user_input != "done": 
+        num_list.append(int(user_input))
+        user_input = input()
+print("Your list is of numbers is: ")
+print(num_list)
+print("Your maximum number is: ")
+print(max_num)
