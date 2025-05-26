e90e1cc569a566e24db8de7ecc09f224dea882fc
Hajer603 <hajeralhattali1@gmail.com>
2025-05-26 08:46:26 +0400

    Solved!

--- examples/from Hajer/Number Sequence Generator.py
+++ examples/from Hajer/Number Sequence Generator.py
@@ -1,4 +1,4 @@
-rows=int(input("Enter the number of rwos = "))
+rows=int(input("Enter the number of rows = "))
 
 for row in range(1, rows + 1):
 
---
+++ examples/from Hajer/friendly greeting bot.py
@@ -0,0 +1,3 @@
+#friendly greeting bot
+name=(input("Enter your name : "))
+print("Hello ",name,"! Welcome to the team. We're glad to have you!")
