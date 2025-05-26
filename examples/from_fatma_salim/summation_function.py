b3b76f7749ba977571adad0e82eaa0d8bf807501
tomh979 <engineertomh@gmail.com>
2025-05-26 11:28:35 +0400

    done

---
+++ examples/from_fatma_salim/summation_function.py
@@ -0,0 +1,23 @@
+
+def sum_list_elements (data_points): 
+    total_sum=0  
+    for num in data_points :
+       total_sum+= num
+    return total_sum 
+
+
+choice = input("Enter 1 to input numbers, or 2 to use an empty list: ")
+
+if choice == "1":
+    user_input = input("Enter numbers separated by commas: ")
+    numbers = [float(x) for x in user_input.split(',')] # to spilt the numbers
+    print("Sum is:", sum_list_elements(numbers))
+
+elif choice == "2":
+    empty_list = []
+    print("Sum is:", sum_list_elements(empty_list))
+
+else:
+    print("Invalid choice. Please enter 1 or 2.")
+
+
