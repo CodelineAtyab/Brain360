392b8d367c3e376c5374996cb2e8e7b8a8307caa
Quds-Aljabri <qudsahmedaljabri@gmail.com>
2025-05-26 14:11:27 +0400

    Deleteing the addition files

--- examples/from_quds/triangle_pattern.py
+++
@@ -1,9 +0,0 @@
-# Task 1 Print Triangle Pattern
-num_of_rows = int(input("Enter the number of rows: ")) 
-if num_of_rows > 0:
-    for count in range(num_of_rows):                      
-        for inner_counr in range(count + 1):
-            print("1", end="")
-        print("")
-else:
-    print("invalid input")
