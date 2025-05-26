08aa284b4453823dfde71ec6658a4826d6cc15ae
tomh979 <engineertomh@gmail.com>
2025-05-26 16:33:55 +0400

    updated

--- examples/from_fatma_salim/element_creat.py
+++ examples/from_fatma_salim/element_creat.py
@@ -1,16 +1,21 @@
-def count_element_occurr(data_list, element_to_count):
+def count_element_occurrences(data_list, element_to_count):
     count = 0
     for item in data_list:
         if item == element_to_count:
             count += 1
     return count
 
+print("Let's build your list one item at a time.")
+data_list = []
 
-user_input = input("Enter list items separated by commas : ") #enter word without space only comma
-data_list = user_input.split(',') # spilt between word by comma 
+n = int(input("How many items do you want in the list? "))
+
+
+for i in range(n):
+    item = input(f"Enter item {i+1}: ")
+    data_list.append(item)
+
 
 element_to_count = input("Enter the element you want to count: ")
 
-result = count_element_occurr(data_list, element_to_count) # call function
-
-print("Output:", result)
+print("Output:", count_element_occurrences(data_list, element_to_count))
