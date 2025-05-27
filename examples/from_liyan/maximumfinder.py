b568e7e3842ddbf835bd3a6319c7dcfdcab9cdf4
liyanalyahmadi12 <layanalyahmadie@gmail.com>
2025-05-27 15:15:33 +0400

    master this is my maximum number finder

---
+++ examples/from_liyan/maximumfinder.py
@@ -0,0 +1,25 @@
+
+#delartion
+def finding_max_value(num):
+    if not num:
+        return None  
+
+#processing 
+    max_value = num[0]
+
+   
+    for value in num[1:]:
+        if value > max_value:
+            max_value = value
+
+    return max_value
+
+scores = [70, 90, 80, 30, 50]
+temperatures = [-1, -8, -10, -2, -3]
+
+maxscore = finding_max_value(scores)
+maxtemp = finding_max_value(temperatures)
+#print
+print("max is: ", maxscore) 
+print("max is: ", maxtemp)
+
