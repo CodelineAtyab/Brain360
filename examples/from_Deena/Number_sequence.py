8649427789e7ef07ba12e45975aa74131bb5652a
deena <deealbusaidi@gmail.com>
2025-05-27 15:59:09 +0400

    Updated Number Sequence

--- examples/from_Deena/Number_sequence.py
+++ examples/from_Deena/Number_sequence.py
@@ -1,8 +1,10 @@
 number = int (input ("Input: "))
-
-for a in range (1, number + 1):
-    for b in range (1, a + 1 ):
-        print (b, end= '')
-    for b in range (a - 1, 0, -1 ):
-        print (b, end='')
-    print('')
+if number > 0:
+    for a in range (1, number + 1):
+        for b in range (1, a + 1 ):
+            print (b, end= '')
+        for b in range (a - 1, 0, -1 ):
+            print (b, end='')
+        print('')
+else:
+    print("Invalid Input")
