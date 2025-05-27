9b06e8773eb6295d316ffb501353140734d60b16
fatma0110 <fatmaalbalushi0110@gmail.com>
2025-05-27 10:26:04 +0400

    updated

--- examples/from_fatma/max_value.py
+++ examples/from_fatma/max_value.py
@@ -1,15 +1,17 @@
 list_of_nums=[]
 def maximum_value(list_of_nums):
-    n = int(input("enter the length of the list: "))
-    for i in range(n):
-        num = int(input("Enter a number: "))
-        list_of_nums.append(num)
-    if list_of_nums==[]:
-        return None
-    else:
-        max_value=list_of_nums[0]
-        for num in list_of_nums:
-            if num>max_value:
-                max_value = num
-        print(max_value)
+    while True:
+        num = input("Enter a number, enter (done) to exit: ")
+        if num == 'done':
+            if list_of_nums==[]:
+                return None
+            else:
+                print(max_value)
+                break
+        else:
+            list_of_nums.append(int(num))
+            max_value=list_of_nums[0]
+            for num in list_of_nums:
+                if num>max_value:
+                    max_value = num      
 maximum_value(list_of_nums)
