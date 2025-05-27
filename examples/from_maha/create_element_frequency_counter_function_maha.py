3e6fe5127a084c1edd38974956af5ae5992e41c9
mahayhmadi <mahayhmadi@gmail.com>
2025-05-27 16:15:00 +0400

    new version is ready to review

--- examples/from_maha/create_element_frequency_counter_function_maha.py
+++ examples/from_maha/create_element_frequency_counter_function_maha.py
@@ -1,6 +1,6 @@
 # Create Element Frequency Counter Function
 my_data = []
-element_to_count = ""
+element_to_count = input("Enter the element you want to count ")
 while True:
         data = input("Enter a list of alphabitics and Done when you finish ")
         if data.lower() == "done":
@@ -11,9 +11,9 @@ while True:
 def count_element_occurrences(Numbers,element):
     count = 0 
     for element in Numbers:
-       if element == "a":
+       if element == element_to_count:
         count = count + 1 
     return  count
-print("Total a is",count_element_occurrences(my_data,element_to_count))
+print("Total", element_to_count, "is",count_element_occurrences(my_data,element_to_count))
 
     
