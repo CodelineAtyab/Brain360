86585dd831d6d1ebb00798ff579c70d3e035522c
liyanalyahmadi12 <layanalyahmadie@gmail.com>
2025-05-27 10:39:49 +0400

    Sorry but this time I really fixed it :,(

--- examples/from_liyan/evennum.py
+++ examples/from_liyan/evennum.py
@@ -1,15 +1,28 @@
+def get_even_numbers(numbers):
+    """
+    This function takes a list of integers and returns a new list containing only the even numbers.
+    The input list remains unchanged.
+    """
+    even_numbers = []
+    for num in numbers:
+        if num % 2 == 0:
+            even_numbers.append(num)
+    return even_numbers
+
+
 list_of_num = []
 
 while True:
-    i_said = input("Write down numbers then enter 'exist' to view even numbers: ")
+    i_said = input("Write down numbers and enter 'exit' to view even numbers: ")
     
-    if i_said == 'exist':
+    if i_said == 'exit':
         break
     
     number = int(i_said)
     list_of_num.append(number)
-    print("Please enter a valid number or 'exist' to quit.")
+    print("Please enter a valid number or 'exit' to quit.")
 
-for num in list_of_num:
-    if num % 2 == 0:
-        print("Even number:", num)
+even_numbers_list = get_even_numbers(list_of_num)
+
+for num in even_numbers_list:
+    print("Even number:", num)
