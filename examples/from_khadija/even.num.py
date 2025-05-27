d70f74184e72c8cc2ee800673a82c73226f3b8e2
khadija829 <khadijazad313@gmail.com>
2025-05-27 08:27:43 +0400

    even numbers task is done

---
+++ examples/from_khadija/even.num.py
@@ -0,0 +1,16 @@
+# Declaration
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# Process
+def get_even_numbers(nums):
+    evens = []
+    for n in nums:
+        if n % 2 == 0:
+            evens.append(n)
+    return evens
+
+even_numbers = get_even_numbers(numbers)
+
+# Output
+print(even_numbers)     # [2, 4, 6, 8, 10]
+print(numbers)          # [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
