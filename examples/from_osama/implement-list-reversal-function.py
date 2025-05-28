e1d9f061523e2584e6e6d06326015b08305080fe
Osama Said Al Zakwani <Osama.Zakwani@gmail.com>
2025-05-28 11:04:31 +0400

    Updated and changed have been done

--- examples/from_osama/implement-list-reversal-function.py
+++ examples/from_osama/implement-list-reversal-function.py
@@ -1,8 +1,7 @@
-database = []
-reverse_database = []
-
-def reverse(database):
-    for i in database[::-1]:
+databaselist = []
+def reverse(databaselist):
+    reverse_database = []
+    for i in databaselist[::-1]:
         reverse_database.append(i)
     return reverse_database
 
@@ -10,12 +9,13 @@ while True:
     inputted_values = input("Enter the values and when you are done please write (Done): ").lower()
 
     if inputted_values != "done":
-        database.append(inputted_values)
+        databaselist.append(inputted_values)
 
-    elif inputted_values == "done" and  len(database) == 0:
+    elif inputted_values == "done" and  len(databaselist) == 0:
         print("Empty")
         break        
 
     elif inputted_values == "done":
-        print(reverse(database))
+        print(databaselist)
+        print(reverse(databaselist))
         break
