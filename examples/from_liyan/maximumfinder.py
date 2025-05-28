a668fdbf7aa3be4d182dab27749d4855930efff2
liyanalyahmadi12 <layanalyahmadie@gmail.com>
2025-05-28 08:29:09 +0400

    fixed issue The function must accept one argument: a list of numbers

--- examples/from_liyan/maximumfinder.py
+++ examples/from_liyan/maximumfinder.py
@@ -1,25 +1,25 @@
-
-#delartion
 def finding_max_value(num):
+   
     if not num:
-        return None  
+        return None
 
-#processing 
+
     max_value = num[0]
 
-   
     for value in num[1:]:
         if value > max_value:
             max_value = value
 
     return max_value
 
-scores = [70, 90, 80, 30, 50]
-temperatures = [-1, -8, -10, -2, -3]
 
-maxscore = finding_max_value(scores)
-maxtemp = finding_max_value(temperatures)
-#print
-print("max is: ", maxscore) 
-print("max is: ", maxtemp)
+user_input = input("Enter a list of numbers separated by spaces then press enter to view the maximum: ")
+
+numbers = [float(num) for num in user_input.strip().split()]
+max_value = finding_max_value(numbers)
+if max_value is not None:
+    print("The maximum value is:", max_value)
+else:
+    print("The list is empty!!!")
+
 
