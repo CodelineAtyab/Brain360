40838acafff16f7194644b165c6070a3e7bd0c79
Hajer603 <hajeralhattali1@gmail.com>
2025-05-28 08:52:08 +0400

    corrected!

--- examples/from-Hajer/triangle_print.py
+++ examples/from-Hajer/triangle_print.py
@@ -1,6 +1,6 @@
 #Print triangular pattern
 
-num_of_rows =6
+num_of_rows =int(input("Enter the number of rows:"))
 for count in range (num_of_rows):
     for inner_count in range(count+1):
         print("*",end=" ")
