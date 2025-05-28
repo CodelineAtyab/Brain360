395577becbc28a9f9019f2007b2c837f19898f32
Hajer603 <hajeralhattali1@gmail.com>
2025-05-28 08:57:50 +0400

    Delete examples/from-Hajer/triangle_print.py

--- examples/from-Hajer/triangle_print.py
+++
@@ -1,7 +0,0 @@
-#Print triangular pattern
-
-num_of_rows =int(input("Enter the number of rows:"))
-for count in range (num_of_rows):
-    for inner_count in range(count+1):
-        print("*",end=" ")
-    print("")  
