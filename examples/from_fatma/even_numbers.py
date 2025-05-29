cf519aae0d230d2bafe066e6a59647edea13f28c
fatma0110 <fatmaalbalushi0110@gmail.com>
2025-05-29 11:14:24 +0400

    updated

--- examples/from_fatma/even_numbers.py
+++ examples/from_fatma/even_numbers.py
@@ -1,17 +1,22 @@
-numbers=[]
+numbers = []
+
+while True:
+    num = input("Enter a number or ( done ) to exit: ")
+    if num != "done":
+        numbers.append(int(num))
+    else:
+        break
+
 def even_number(numbers):
-    n = int(input("enter the length of the list: "))
-    for i in range(n):
-        num = int(input("Enter a number: "))
-        numbers.append(num)
-    if numbers==[]:
+    if numbers == []:
         print("The list is empty")
         return None
     else:
-        even_num=[]
-        for num in numbers:
-            if num % 2 == 0:
-                even_num.append(num)
-        print(even_num)
-        return even_num
+        even_numbers = []
+        for n in numbers:
+            if n % 2 == 0:
+                even_numbers.append(n)
+        print(f"even numbers: {even_numbers}")        
+        return even_numbers
+
 even_number(numbers)
