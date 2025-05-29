73fde93e71320d537feaa778a0730aa03f62e3c6
fatma0110 <fatmaalbalushi0110@gmail.com>
2025-05-29 12:23:26 +0400

    updated

--- examples/from_fatma/even_numbers.py
+++ examples/from_fatma/even_numbers.py
@@ -6,18 +6,16 @@ while True:
         numbers.append(int(num))
     else:
         break
-    
-even_numbers = []
 
 def even_number(even_numbers):
-    if numbers == []:
-        print("The list is empty")
-        return None
-    else:
-        for n in numbers:
-            if n % 2 == 0:
-                even_numbers.append(n)
-        print(f"even numbers: {even_numbers}")        
-        return even_numbers
+    listOfeven=[]
+    for n in even_numbers:
+      if n % 2 == 0:
+        listOfeven.append(n) 
+    return listOfeven       
+             
+   
+   
 
-even_number(even_numbers)
+print("The list",numbers)
+print("The list ot even " , even_number(numbers))
