c9776bdbebdf29259c235656606fa42b4fdc2120
Duaa <124520079+Duaa3@users.noreply.github.com>
2025-05-30 19:17:20 +0400

    AllDataTypes

--- examples/from_duaa/frequency.py
+++ examples/from_duaa/frequency.py
@@ -10,5 +10,6 @@ userList = [item.strip() for item in userInput.split(",")]
 element = input("What is the element you like to count?  ")
 
 print("The Total of " , element , "is: ", frequency(userList,element))
-
-
+print("Testing if the fun work with all data Typs: ")
+print("The list is :",[True,True,False], "And the element to count is: True")
+print(frequency([True,True,False],True))
