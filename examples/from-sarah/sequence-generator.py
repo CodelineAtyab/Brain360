48006c6031ad66b8df606d70e0e7c6872c3a2f35
Sarah-Alabbadi7 <sarahala156@gmail.com>
2025-06-01 11:57:00 +0400

    Delete

--- examples/from-sarah/sequence-generator.py
+++
@@ -1,9 +0,0 @@
-n = int(input("number of rows!: "))
-for i in range(1, n + 1):
-    # Increasing part
-    for j in range(1, i + 1):
-        print(j, end="")
-    # Decreasing part
-    for j in range(i - 1, 0, -1):
-        print(j, end="")
-    print()
