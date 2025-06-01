50b615941c3c4320c466ea98b410ee09197f3248
AlzahraAhmed9 <alzahra.alberiki@gmail.com>
2025-06-01 10:31:38 +0400

    modified

--- examples/from_alzahra/reversel_function.py
+++ examples/from_alzahra/reversel_function.py
@@ -1,17 +1,18 @@
 list_rev=[]
 
+while True:
+    x=input("Enter an element : ")
+    if x=="done":
+        break
+    else:
+        list_rev.append(x)
 
-def list_of_rev(lis_rev):
 
-    while True:
-        x=input("Enter an element : ")
-        if x=="done":
-            break
-        else:
-            list_rev.append(x)
+def list_of_rev(list_rev):
     new_list=[]
     for i in list_rev[::-1]:
         new_list.append(i)
-    return new_list 
+    return new_list
 
+print("The original list: ",list_rev)
 list_of_rev(list_rev)
