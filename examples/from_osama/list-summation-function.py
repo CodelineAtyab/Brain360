419499ee159120eede23c587ce5ee6429381f397
Osama Said Al Zakwani <Osama.Zakwani@gmail.com>
2025-06-01 14:45:54 +0400

    List summation Task Donr

---
+++ examples/from_osama/list-summation-function.py
@@ -0,0 +1,16 @@
+database = []
+
+def sum_list_elements(database):
+    sum_all = sum(database)             
+    return sum_all
+
+it_is_sum = True
+
+while it_is_sum:
+    inputted_numbers = input("Please enter the a number whole or float number BUT do not enter any strig when you done please enter (Done): ").lower()
+    if inputted_numbers != "done":
+        database.append(float(inputted_numbers))
+        
+    if inputted_numbers == "done":
+        print(f"The Total is:", sum_list_elements(database))
+        break
