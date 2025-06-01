a011b2b3de710e783941a6b08d42f6f3e94247fb
maather999 <maatheralwadhahi1999@gmail.com>
2025-06-01 10:53:46 +0400

    update on number_sequence

--- examples/from_maather/number_sequence.py
+++ examples/from_maather/number_sequence.py
@@ -3,7 +3,7 @@ if user_input > 0:
     for i in range(1,user_input+1) :
         for j in range(1,i+1):
             print(j,end=" ")
-        for j in range(1,i-1):
+        for j in range(i-1,0,-1):
             print(j,end=" ")
         print()
 else:
