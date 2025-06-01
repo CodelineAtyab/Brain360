ca4e4bdad845e016021517b84d7b556e85e731de
fatma0110 <fatmaalbalushi0110@gmail.com>
2025-06-01 08:36:52 +0400

    modified

--- examples/from_fatma/sum_function.py
+++ examples/from_fatma/sum_function.py
@@ -8,9 +8,9 @@ while True:
             print("List:",list1)
             break
 
-def sum_of_list(list1):
+def sum_of_list(lst):
     total = 0
-    for n in list1:
+    for n in lst:
         total += n
     print(total)
     return total
