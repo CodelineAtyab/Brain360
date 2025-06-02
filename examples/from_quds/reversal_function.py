7819be8706295ef697e329f6b6c8e385ff904080
Quds-Aljabri <qudsahmedaljabri@gmail.com>
2025-06-02 16:51:59 +0400

    reversal function

---
+++ examples/from_quds/reversal_function.py
@@ -0,0 +1,32 @@
+my_list = []
+new_list = []
+
+while True:
+      user_input = input("Enter the elemnts write [done ] when you finish: ")
+      if user_input != "done":
+          my_list.append(user_input)
+      else:
+          break
+
+
+def reverse_my_list(my_list):
+     new_list = my_list[::-1]
+     print(new_list)
+
+reverse_my_list(my_list)     
+print(my_list)
+
+# def reverse_my_list(my_list):
+#      new_list = my_list[::-1]
+#      print(new_list)
+
+# reverse_my_list(my_list)
+
+
+
+
+
+
+
+       
+
