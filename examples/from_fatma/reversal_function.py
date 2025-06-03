9f74926522eb5813f6e10dc3f02a749f7b86c147
fatma0110 <fatmaalbalushi0110@gmail.com>
2025-06-03 08:31:41 +0400

    file modified

--- examples/from_fatma/reversal_function.py
+++ examples/from_fatma/reversal_function.py
@@ -5,15 +5,16 @@ while True:
     if data != "done":
         my_list.append(data)
     else:
+        print("The original list: ",my_list)
         break
 
 def reverse_func(lst):
     if lst ==[]:
-        print(my_list)
+        print("your list is empty.")
     else:
         new_list=[]
         for i in lst[::-1]:
             new_list.append(i)
-        print(new_list)
+        print("The reversed list: ",new_list)
 
 reverse_func(my_list)
