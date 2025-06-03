a907263136cb05a3f6e2168e0f0618b53f2e4afa
unknown <sarahala156@gmsil.com>
2025-06-03 15:58:26 +0400

    Add staircase.py

---
+++ examples/from-sarah/staircase.py
@@ -0,0 +1,9 @@
+
+num_of_rows = int(input("Enter number of rows: "))
+
+if num_of_rows > 0:                   #checks if the number is positive.
+    for i in range(1, num_of_rows + 1):
+        print(' ' * (num_of_rows - i) + '#' * i)
+else:
+    print("Please enter a positive number.")
+
