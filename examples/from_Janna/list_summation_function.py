c08851225fe47374155b5281eded9104265e6731
Janna-Khalid <janna.balushi@outlook.com>
2025-06-03 13:55:46 +0400

    added summation func

--- examples/from_Janna/list_summation_function.py
+++ examples/from_Janna/list_summation_function.py
@@ -7,10 +7,13 @@ def user_input():
         list_of_values.append(float(i))
     return list_of_values 
 
-def sum_value():
-    return sum(list_of_values)
+def sum_value(numbers):
+    total = 0
+    for num in numbers:
+        total += num
+    return total
 
 user_input()
 
 print(f"List: {list_of_values}")
-print(f"The Sum is {sum_value()}")
+print(f"The Sum is {sum_value(list_of_values)}")
