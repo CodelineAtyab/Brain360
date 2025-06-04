d14587db80d41295ddf10f7cc1bd283eb2da3f87
Adham <eng.adhamalsiniadi@gmail.com>
2025-06-04 12:59:00 +0400

    modefied max value file

--- examples/from_adham/max_vlaue_finder.py
+++ examples/from_adham/max_vlaue_finder.py
@@ -8,18 +8,18 @@ def find_max_value(numbers):
             biggest = num
     return biggest
 
-
 data = input("Put some numbers separated by commas: ")
-items = data.split(",")
-nums = []
 
-for n in items:
-    nums.append(int(n)) 
-
-
-result = find_max_value(nums)
-
-if result is None:
-    print("The list was empty.")
+if data == "":
+    print("The list is empty.")
+    nums = []
 else:
+    nums = []
+    parts = data.split(",")
+    for i in parts:
+        nums.append(int(i))
+
+    result = find_max_value(nums)
     print("The biggest number is:", result)
+
+print("Original list:", nums)
