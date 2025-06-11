3bb4264ea7925ae12a5904ba81a11fd0c1f301cb
Quds-Aljabri <qudsahmedaljabri@gmail.com>
2025-06-11 07:55:04 +0400

    Square Constellation Creator

---
+++ examples/from_quds/square_creator.py
@@ -0,0 +1,7 @@
+square_star = int(input("Please enter a positive number: "))
+if square_star > 0:
+    for i in range(square_star):
+        print("*"*square_star)
+else:
+    print("Invalid Input")
+    
