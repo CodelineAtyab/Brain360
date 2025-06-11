760cc6b552b8569365f92c67137c0e8c69db07e1
deena <deealbusaidi@gmail.com>
2025-06-11 08:36:07 +0400

    updating stairway to stars

--- examples/from_Deena/stairway.py
+++ examples/from_Deena/stairway.py
@@ -1,3 +1,6 @@
-number = int(input("Enter a number: "))
-for i in range (1, number + 1):
-    print (i * "*")
+number = int(input("Enter a positive number: "))
+if number < 0:
+    print ("No negative numbers accepted")
+else:
+    for i in range (1, number + 1):
+        print (i * "*")
