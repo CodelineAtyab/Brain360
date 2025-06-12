f8dac717de438dad9f210980c1093ffcafd32e28
mahayhmadi <mahayhmadi@gmail.com>
2025-06-12 11:02:30 +0400

    updated file last version

--- examples/from_maha/square_constellation_creator.py
+++ examples/from_maha/square_constellation_creator.py
@@ -1,7 +1,7 @@
 ask_user = int(input("enter your desired number of rows: "))
 if ask_user > 0:
     for i in range(ask_user):
-        print("*" * 4)
+        print("*" * ask_user)
 else:
     print("Wrong!, Enter a positive number!")
 
