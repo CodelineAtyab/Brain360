20515fa4657666eb58e083dcf215b93f0ec8ff39
unknown <shihab.ahmed.aljassasi@gmail.com>
2025-06-12 10:11:28 +0400

    adding a new task (square-constellation)

---
+++ examples/From_shihab/square-constellation.py
@@ -0,0 +1,13 @@
+# user_input = int(input("Enter Number Of Row "))
+
+while True:
+    user_input = int(input("Enter Number Of Row "))
+
+    if user_input <=0:
+        print("Please Enter Number Greater Than 0 ")
+    for r in range(user_input):
+        number ="*" * (user_input)
+        print(number)
+    break
+
+    
