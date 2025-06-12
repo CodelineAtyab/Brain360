62c020d238ee04156f4df7c27fd4b31f5327916b
unknown <ahmedalnaimee44@gmail.com>
2025-06-12 14:38:41 +0400

    86etryxwr-Core-Feedback-Management-System

--- examples/from_ahmed/Core_Feedback_Management_System/Core_Feedback_Management_System.py
+++ examples/from_ahmed/Core_Feedback_Management_System/Core_Feedback_Management_System.py
@@ -6,17 +6,17 @@ fblist = []
 
 
 while True:
-    print("Type '1' to add a feedback")
-    print("Type '2' to show the list of feedbacks")
+    print("Type '1' to show the list of feedbacks")
+    print("Type '2' to add a feedback")
     print("Type '3' to update a feedback")
     print("Type '4' to remove a feedback")
     print("Type 'done' to exit the")
 
     menu = input()
     if menu ==  "1":
-        addfb(fblist)
-    if menu == "2":
         viewfb(fblist)
+    if menu == "2":
+        addfb(fblist)
     if menu == "3":
         upfb(fblist)
     if menu == "4":
