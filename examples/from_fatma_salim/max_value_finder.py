45975a7a3bb27fa754489da7516b1dc853471f30
tomh979 <engineertomh@gmail.com>
2025-06-17 15:17:52 +0400

    updated

--- examples/from_fatma_salim/max_value_finder.py
+++ examples/from_fatma_salim/max_value_finder.py
@@ -3,19 +3,25 @@ def find_max_value(numbers):
     if not numbers:
         return None
 
-   
     max_value = numbers[0]
-
-    for num in numbers[1:]:
-        if num > max_value:
-            max_value = num
+    for number in numbers[1:]:
+        if number > max_value:
+            max_value = number
 
     return max_value
 
-scores = [78, 92, 56, 99, 85]
-temperatures = [-2, -8, -1, -5]
-no_values = []
 
-print(find_max_value(scores))        
-print(find_max_value(temperatures))  
-print(find_max_value(no_values))     
+user_input = input("Enter numbers separated by spaces: ")
+
+try:
+    number_list = [float(num) for num in user_input.split()]
+except ValueError:
+    print("Please enter only valid numbers.")
+    exit()
+
+max_number = find_max_value(number_list)
+
+if max_number is not None:
+    print("The maximum number is:", max_number)
+else:
+    print("You entered an empty list.")
