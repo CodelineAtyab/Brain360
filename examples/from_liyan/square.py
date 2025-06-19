782275011beba1e8c285f934ce5d2cf9b38d6666
liyanalyahmadi12 <layanalyahmadie@gmail.com>
2025-06-19 09:07:28 +0400

    Sqaure Pattren Program Ready For Review (#367)

---
+++ examples/from_liyan/square.py
@@ -0,0 +1,10 @@
+"This Program Takes a Positive Integer And Turns it Into A Sqaure Pattren"
+
+
+n= int(input("Write down a positive intger: "))
+
+
+for i in range(n):
+    for j in range(n):
+     print("*", end="")
+    print()
