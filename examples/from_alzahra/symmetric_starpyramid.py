ccdf638b92c4d4d9907b6aadd60ac284b8da114c
AlzahraAhmed9 <alzahra.alberiki@gmail.com>
2025-06-23 12:09:13 +0400

    Symmetric Star Pyramid (#395)
    * Task Is Done
    
    * Updated

---
+++ examples/from_alzahra/symmetric_starpyramid.py
@@ -0,0 +1,10 @@
+num_of_star= int(input("Enter a number (the number should be ve+): "))
+
+for i in range (1, num_of_star+1): #generate a sequance of numbers starting from 1 to num_of_star 
+    if num_of_star<0:
+        print("the number should be positive")
+    else:
+        sp=num_of_star-i
+        st=2*i-1
+        line= " " * sp + "*" * st 
+        print(line)
