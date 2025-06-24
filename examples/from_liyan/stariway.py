f39be182e30e00f81fde469268f6bdf35e44d626
liyanalyahmadi12 <layanalyahmadie@gmail.com>
2025-06-24 10:10:15 +0400

    just fixed the issue with the numbers not matching should work just fine

--- examples/from_liyan/stariway.py
+++ examples/from_liyan/stariway.py
@@ -1,10 +1,8 @@
 "This Program Takes a Positive Integer and Turns It Into a Stairway Like Shape"
 
+n = int(input("Please write down a positive intger: "))
 
-
-n = int(input("write a positive integer: "))
-
-for i in range( n - 1):
+for i in range(n):
     for j in range(i + 1 ):
-        print("*", end=" " )
+        print("*", end=" ")
     print()
