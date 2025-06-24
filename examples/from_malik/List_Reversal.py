57ae7ec205d292d4a4e129105b8f775ba4570aad
Malik <malek_noor40@hotmail.com>
2025-06-24 15:17:10 +0400

    DONE

--- examples/from_malik/List_Reversal.py
+++ examples/from_malik/List_Reversal.py
@@ -3,7 +3,8 @@ def reverse_my_list(input_list):
 
 user_input = input("Enter list elements separated by commas: ")
 
+
 input_list = [item.strip() for item in user_input.split(',') if item.strip()]
 
 print("Original List:", input_list)
-print("Reversed List:", reverse_my_list(input_list))
+print("Reversed List:", reverse_my_list(input_list)) 
