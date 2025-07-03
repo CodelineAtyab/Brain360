a3950579cdcc110019473423709b54000d67612e
Malik <malek_noor40@hotmail.com>
2025-07-03 10:16:40 +0400

    done2

--- examples/from_malik/max_num_finder.py
+++ examples/from_malik/max_num_finder.py
@@ -1,13 +1,16 @@
-numbers = []
+def find_max_value_from_input():
+    numbers = [] 
 
-user_input = input("Enter numbers separated by commas: ")
+    user_input = input("Enter numbers separated by commas: ")
 
-for x in user_input.split(','):
-    numbers.append(int(x.strip()))
+    for x in user_input.split(','):
+        numbers.append(int(x.strip()))
 
-max_value = numbers[0]
-for num in numbers:
-    if num > max_value:
-        max_value = num
+    max_value = numbers[0]
+    for num in numbers:
+        if num > max_value:
+            max_value = num
 
-print("The maximum number is:", max_value)
+    print("The maximum number is:", max_value)
+
+find_max_value_from_input()
