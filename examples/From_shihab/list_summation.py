4660e8ec613be210f91be8fc8bf64ca19b03ff4d
unknown <shihab.ahmed.aljassasi@gmail.com>
2025-07-04 21:49:27 +0400

    faxing list_summation task

--- examples/From_shihab/list_summation.py
+++ examples/From_shihab/list_summation.py
@@ -2,11 +2,11 @@ def sum_list_elements(number_list):
     return sum(number_list)
 
 
-print("Enter numbers one by one. Type 'done' when finished:")
+
 
 number_list = []
 while True:
-    num = input("Number: ")
+    num = input("Enter numbers one by one. Type 'done' when finished:")
     if num.lower() == "done":
         break
     number_list.append(float(num))  
